425f5da9cc1bdc03e1d981d01f443470af5677ac
MrRyhor <125206451+MrRyhor@users.noreply.github.com>
2023-03-30 21:11:29 +0200

    lesson15 correct task0

--- tasks/js/script0.js
+++ tasks/js/script0.js
@@ -1,21 +1,17 @@
 let obj1 = {
     numbers: [2, 7, 21, 12, 34, 56, 11, 9, 45, 13, 28, 12, 1, 3],
-    getSum: function (min, max) {
-        this.numbers.sort((el1, el2) => el1 - el2)
-        return this.numbers.reduce((prevSum, element) => element <= max && element > min ? prevSum + element : prevSum, min)
+    getSum: function () {
+        return this.numbers.reduce((prevSum, element) => prevSum + element)
     }    
 }
 
 let obj2 = {
     numbers: [21, 14, 2, 20, 3, 67, 1, 12, 11, 32, 6, 30, 8, 10],
     getMult: function (min, max) {
-        this.numbers.sort((el1, el2) => el1 - el2)
-        return this.numbers.reduce((prevMult, element) => element > min && element <= max ? prevMult * element : prevMult, min)
+        return this.numbers.reduce((prevMult, element) => element >= min && element <= max ? prevMult * element : prevMult, 1)
     }
 }
 
-console.log(obj1)
-console.log(obj2)
 document.write(`<p>Sum of Obj1 = ${obj1.getSum(7, 28)}</p>`)
 document.write(`<p>Mult of Obj2 = ${obj2.getMult(6, 14)}</p>`)
 
